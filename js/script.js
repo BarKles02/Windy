3a1a7c2bc1413cc83d5139952642737cc9149648
BarKles02 <bartlomiejkleszczewski@gmail.com>
2022-10-10 18:00:41 +0200

    functions update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
-console.log("Pozdrawiam Klubowiczów. Ten kod jest już w repozytorium .Git");
+{
+    const welcome = () => {
+        console.log("Pozdrawiam Klubowiczów. Ten kod jest już w repozytorium .Git");
+    }
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".js-themeName")
+        body.classList.toggle("body--darkMode");
+        if (body.classList.contains("body--darkMode")) {
+            themeName.innerText = "jasny";
+        } else {
+            themeName.innerText = "ciemny";
+        }
 
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let themeName = document.querySelector(".js-themeName")
+    }
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackground);
 
-button.addEventListener("click", () => {
-    body.classList.toggle("body--darkMode");
-    if (body.classList.contains("body--darkMode")) {
-        themeName.innerText = "jasny";
-    } else {
-        themeName.innerText = "ciemny";
+        welcome();
     }
 
-});
-
+    init();
+}
